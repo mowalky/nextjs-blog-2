792614d437ca0fe903095632df59bd14e868586a
Mowalky <mowalky@outlook.com>
2022-06-02 07:23:03 -0500

    post item

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,3 +1,34 @@
-function PostItem() {}
+import classes from "./post-item.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+function PostItem(props) {
+  const { image, title, date, excerpt, slug } = props.post;
+
+  const formattedDate = new Date(date).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  const imagePath = `/images/posts/${slug}/${image}`;
+
+  return (
+    <li className={classes.post}>
+      <Link href={slug}>
+        <a>
+          <div className={classes.image}>
+            <Image src={imagePath} alt={title} width={300} height={200} />
+          </div>
+          <div className={classes.content}>
+            <h3>{title}</h3>
+            <time>{formattedDate}</time>
+            <p>{excerpt}</p>
+          </div>
+        </a>
+      </Link>
+    </li>
+  );
+}
 
 export default PostItem;
