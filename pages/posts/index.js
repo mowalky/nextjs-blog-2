209aaa2bb4e779a76ed9e2b0cb3bd1ef7b287ece
Mowalky <mowalky@outlook.com>
2022-06-03 22:27:45 -0500

    add posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,26 @@
-function AllPostsPage() {}
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_DATA = [
+  {
+    slug: "post-1",
+    title: "Post 1",
+    date: "2020-01-01",
+    excerpt:
+      "NextJS is the best framework for building server side applications",
+    image: "yoda.jpg",
+  },
+  {
+    slug: "post-2",
+    title: "Post 1",
+    date: "2020-01-01",
+    excerpt:
+      "NextJS is the best framework for building server side applications",
+    image: "r2d2.jpg",
+  },
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_DATA} />;
+}
 
 export default AllPostsPage;
