551b30d7c6c5a166212fca44cbe4e86ec760b4a8
Mowalky <mowalky@outlook.com>
2022-06-04 09:27:31 -0500

    updated post details

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,9 +1,19 @@
 import PostHeader from "./post-header";
 
+const DUMMY_POST = {
+  slug: "post-1",
+  title: "Post 1",
+  date: "2020-01-01",
+  image: "yoda.jpg",
+  content: "# This is a first post",
+};
+
 function PostContent() {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
   return (
     <article>
-      <PostHeader />
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      {DUMMY_POST.content}
     </article>
   );
 }
