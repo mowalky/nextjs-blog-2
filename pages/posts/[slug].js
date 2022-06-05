2aefc6937bd502ad69a169a8e4b1a800339f8847
Mowalky <mowalky@outlook.com>
2022-06-05 16:37:47 -0500

    updated

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,33 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-function SinglePostPage() {
-  return <PostContent />;
+function SinglePostPage(props) {
+  return <PostContent posts={props.posts} />;
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const postFilesnames = getPostsFiles();
+
+  const slugs = postFilesnames.map((filename) => filename.replace(/\.md$/, ""));
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
 }
 
 export default SinglePostPage;
