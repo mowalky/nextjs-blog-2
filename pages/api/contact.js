376ef33aef4b8ee19b4530bbb2b24edcf2a8f433
Mowalky <mowalky@outlook.com>
2022-06-07 22:42:34 -0500

    connect contact to mongodb

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -8,7 +10,20 @@ export default function handler(req, res) {
       message,
     };
 
-    console.log(newMessage);
+    console.log(process.env.DB_USER);
+    let client;
+    try {
+      client = await MongoClient.connect(
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@nextjs.iimds.mongodb.net/?retryWrites=true&w=majority`
+      );
+    } catch (e) {
+      res.status(500).json({ message: "Error connecting to db" });
+      return;
+    }
+
+    const db = client.db("nextjs");
+    await db.collection("contact").insertOne(newMessage);
+
     res.status(201).json({ message: newMessage });
   }
 }
