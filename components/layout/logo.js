b24496b8b837666ded6b4f3b334a9662157c1d46
Mowalky <mowalky@outlook.com>
2022-06-08 07:23:01 -0500

    added notifications

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,7 +1,7 @@
 import classes from "./logo.module.css";
 
 function Logo() {
-  return <div className={classes.logo}>Next Blog</div>;
+  return <div className={classes.logo}>RED5 Blog</div>;
 }
 
 export default Logo;
